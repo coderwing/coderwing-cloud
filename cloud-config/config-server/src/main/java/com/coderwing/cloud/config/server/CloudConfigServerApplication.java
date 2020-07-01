56209fed992579747d1554073dbb7ff5034cd905
songwenxiong <coderwing@163.com>
2020-07-01 23:04:59 +0800

    cloud config 提交

--- cloud-config/config-server/src/main/java/com/coderwing/cloud/config/server/CloudConfigServerApplication.java
+++ cloud-config/config-server/src/main/java/com/coderwing/cloud/config/server/CloudConfigServerApplication.java
@@ -1,12 +1,16 @@
 package com.coderwing.cloud.config.server;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 // 开启Config Server能力
 @EnableConfigServer
+// 非DB模式启动
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class CloudConfigServerApplication {
 
     public static void main(String[] args) {
