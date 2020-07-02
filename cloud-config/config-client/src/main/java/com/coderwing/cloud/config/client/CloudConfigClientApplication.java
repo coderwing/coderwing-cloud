45577548888a1507913fa1ab84e13742198cf1ec
songxudong <coderwing@163.com>
2020-07-02 13:12:57 +0800

    cloud config client增加服务总线消息通知：用于发送配置文件修改的更新通知（依赖于RabbitMQ）

--- cloud-config/config-client/src/main/java/com/coderwing/cloud/config/client/CloudConfigClientApplication.java
+++ cloud-config/config-client/src/main/java/com/coderwing/cloud/config/client/CloudConfigClientApplication.java
@@ -4,11 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 // 开启Eureka注册
 @EnableEurekaClient
+// 启动服务发现
+@EnableDiscoveryClient
+// 启动配置文件刷新能力
+@RefreshScope
 // 非DB模式启动
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class CloudConfigClientApplication {
