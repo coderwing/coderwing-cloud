15fcc8509d8daef0bc22cd832f3780c0563a6334
songxudong <coderwing@163.com>
2020-07-02 12:43:03 +0800

    cloud config server高可用服务注册发现集群支持

--- cloud-config/config-client/src/main/java/com/coderwing/cloud/config/client/CloudConfigClientApplication.java
+++ cloud-config/config-client/src/main/java/com/coderwing/cloud/config/client/CloudConfigClientApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+// 开启Eureka注册
+@EnableEurekaClient
 // 非DB模式启动
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class CloudConfigClientApplication {
--- cloud-config/config-server/src/main/java/com/coderwing/cloud/config/server/CloudConfigServerApplication.java
+++ cloud-config/config-server/src/main/java/com/coderwing/cloud/config/server/CloudConfigServerApplication.java
@@ -5,10 +5,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 // 开启Config Server能力
 @EnableConfigServer
+// 开启Eureka注册
+@EnableEurekaClient
 // 非DB模式启动
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class CloudConfigServerApplication {
